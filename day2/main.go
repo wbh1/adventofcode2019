e99ec567bf67bba2734e67d7330cbccddef929c5
Will Hegedus <wbhegedus@liberty.edu>
2019-12-03 21:42:16 -0500

    finished day2

--- day2/main.go
+++ day2/main.go
@@ -28,17 +28,11 @@ func main() {
 		numbers[i] = num
 	}
 
-	opcode = numbers[opcodePos]
-	for opcode != 99 {
-		input1 = numbers[numbers[opcodePos+1]]
-		input2 = numbers[numbers[opcodePos+2]]
-		numbers[numbers[opcodePos+3]] = compute(opcode, input1, input2)
-		opcodePos+=4
-		opcode = numbers[opcodePos]
+	numbers = part1(numbers)
+	logrus.Info(numbers[0])
 
-	}
-
-	logrus.Info(numbers)
+	param1, param2 := part2(numbers)
+	logrus.Info(param1, param2)
 }
 
 func compute(opcode, num1, num2 int) int {
@@ -50,4 +44,47 @@ func compute(opcode, num1, num2 int) int {
 		logrus.Fatal(opcode, " is not a valid opcode")
 	}
 	return 0
+}
+
+func part1(numbers []int) []int {
+	opcode = numbers[opcodePos]
+	for opcode != 99 {
+		input1 = numbers[numbers[opcodePos+1]]
+		input2 = numbers[numbers[opcodePos+2]]
+		numbers[numbers[opcodePos+3]] = compute(opcode, input1, input2)
+		opcodePos+=4
+		opcode = numbers[opcodePos]
+	}
+
+	return numbers
+}
+
+func part2(numbers []int) (param1, param2 int) {
+	output := 0
+	for param1 := 0; param1 <= 99 && output != 19690720; param1++ {
+		for param2 := 0; param2 <= 99 && output != 19690720; param2++ {
+			opcodePos = 0
+			numbers = readInput()
+			numbers[1] = param1
+			numbers[2] = param2
+			output = part1(numbers)[0]
+		}
+	}
+	return numbers[1], numbers[2]
+}
+
+func readInput() []int {
+	input, err := ioutil.ReadFile("input.txt")
+	if err != nil {
+		logrus.Fatal(err)
+	}
+
+	strNums := strings.Split(string(input), ",")
+	numbers := make([]int, len(strNums))
+	for i, v := range strNums {
+		num, _ := strconv.Atoi(v)
+		numbers[i] = num
+	}
+
+	return numbers
 }
