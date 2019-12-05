db1980a29743e90012225060af33faf256b5cc19
Will Hegedus <wbhegedus@liberty.edu>
2019-12-05 15:33:54 -0500

    day 3 done

--- day3/main.go
+++ day3/main.go
@@ -10,105 +10,84 @@ import (
 )
 
 type point struct {
-	x int
-	y int
+	x              int
+	y              int
+	distFromOrigin float64
 }
 
 var (
-	line1 []string
-	line2 []string
-	grid  [10001][10001]int
-	xPos  = 5001
-	yPos  = 5001
+	updatePoint func(p *point)
 )
 
 func main() {
-	input, err := ioutil.ReadFile("input.txt")
+
+	bytes, err := ioutil.ReadFile("input.txt")
 	if err != nil {
-		logrus.Error(err)
+		logrus.Error("Error reading file: ", err)
 	}
 
-	directions := helpers.SplitOnNewLines(input)
+	contents := helpers.SplitOnNewLines(bytes)
+	lines := make(map[point][2]float64)
 
-	line1 = make([]string, len(directions[0]))
-	line2 = make([]string, len(directions[1]))
-
-	for i, v := range strings.Split(string(directions[0]), ",") {
-		line1[i] = v
-	}
-
-	for i, v := range strings.Split(string(directions[1]), ",") {
-		line2[i] = v
-	}
-
-	part1()
-}
-
-func part1() {
-	for _, v := range line1 {
-		if v == "" {
-			continue
-		}
-		if xAxis(v) {
-			xPos = xPos + movement(v)
-			grid[xPos][yPos] = 1
-		} else {
-			yPos = yPos + movement(v)
-			grid[xPos][yPos+movement(v)] = 1
-		}
-	}
-
-	for _, v := range line2 {
-		if v == "" {
-			continue
-		}
-		if xAxis(v) {
-			xPos = xPos + movement(v)
-			if grid[xPos][yPos] == 1 {
-				grid[xPos][yPos] += 2
+	for i, line := range contents {
+		curPoint := point{0, 0, 0}
+		traveled := 0.0
+		for _, step := range strings.Split(line, ",") {
+			dist, err := strconv.Atoi(step[1:])
+			if err != nil {
+				logrus.Fatal("Couldn't parse step: ", step)
 			}
-		} else {
-			yPos = yPos + movement(v)
-			if grid[xPos][yPos] == 1 {
-				grid[xPos][yPos] += 2
+
+			switch step[0] {
+			case 'L':
+				updatePoint = func(p *point) {
+					p.x--
+				}
+			case 'R':
+				updatePoint = func(p *point) {
+					p.x++
+				}
+			case 'U':
+				updatePoint = func(p *point) {
+					p.y++
+				}
+			case 'D':
+				updatePoint = func(p *point) {
+					p.y--
+				}
+			default:
+				logrus.Fatal("Uh oh... couldn't parse this step: ", step)
+			}
+
+			for mv := 1; mv <= dist; mv++ {
+				updatePoint(&curPoint)
+				hits := lines[curPoint]
+				if hits[i] != 0 {
+					hits[i] = math.Min(hits[i], float64(mv)+traveled)
+					logrus.Info(hits)
+				} else {
+					hits[i] = float64(mv) + traveled
+				}
+				lines[curPoint] = hits
+			}
+			traveled += float64(dist)
+
+		}
+	}
+	closest := math.MaxFloat64
+	least := math.MaxFloat64
+	for loc, hits := range lines {
+		if hits[0] != float64(0) && hits[1] != float64(0) {
+			distance := math.Abs(float64(loc.x)) + math.Abs(float64(loc.y))
+			if distance < closest {
+				closest = distance
+			}
+			sum := hits[0] + hits[1]
+			if sum < least {
+				least = sum
 			}
 		}
 	}
 
-	for y, row := range grid[:] {
-		for x, v := range row {
-			if v == 3 {
-				logrus.Info(x, y,
-					math.Abs(float64(x)-5001.0)+math.Abs(float64(y)-5001.0))
-			}
-		}
-	}
-}
-
-func xAxis(direction string) bool {
-	if strings.Contains(direction, "L") {
-		return true
-	}
-	if strings.Contains(direction, "R") {
-		return true
-	}
-	return false
-}
-
-func movement(direction string) int {
-	logrus.Info(direction)
-	spacesMoved := []rune(direction)[1:]
-	move, err := strconv.Atoi(string(spacesMoved))
-	if err != nil {
-		logrus.Error(err)
-	}
-
-	if strings.Contains(direction, "L") {
-		return 0 - move
-	}
-	if strings.Contains(direction, "R") {
-		return move
-	}
-
-	return 0
+	logrus.Infof("Part A: %d; Part B: %d", int(closest), int(least))
 }
